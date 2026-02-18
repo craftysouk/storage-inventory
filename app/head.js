42aee864a6cb18409e5c39daca85f1759bc00133
Maritza Pizarro <ritzaaa@Maritzas-MacBook-Air.local>
2026-02-18 10:05:09 -0700

    Add PWA support

---
+++ app/head.js
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="apple-touch-icon" href="/icon-192.png" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    </>
+  );
+}
